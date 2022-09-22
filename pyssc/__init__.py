1e33b319753fd0d1447e3cae265bae38b643cc91
Jan Joschka Wohlgemuth <jj.wohlgemuth@gmail.com>
2022-09-22 16:48:41 +0200

    changed import structure

--- pyssc/__init__.py
+++ pyssc/__init__.py
@@ -25,5 +25,7 @@ except Exception:
     __version__ = "unknown"
 
 
-from . import ssc_device
-from . import ssc_device_setup
+from .scan import scan
+from .ssc_device import Ssc_device
+from .ssc_device_setup import Ssc_device_setup
+
